22c35f51b12a94bae04640677bb9566bce5700ec
aryanpatel10 <aaryanpatel013@gmail.com>
2026-01-28 22:00:37 +0530

    Problem

---
+++ Algorithms/Sorting/02.Insertion_sort/Descending.py
@@ -0,0 +1,17 @@
+# DESCENDING ORDER SORTING
+# input: nums = [5,2,3,1]
+
+nums = [5,2,3,1]
+def insertion_sort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        key  = nums[i]
+        j = i-1
+        while j>=0 and nums[j]<key:
+            nums[j+1] = nums[j]
+            nums[j] = key
+            j = j-1
+
+    return nums
+
+print(insertion_sort(nums))
---
+++ Algorithms/Sorting/03.Selection_sort/Descending.py
@@ -0,0 +1,21 @@
+def selection_sort(nums):
+    n = len(nums)
+
+    for i in range(n):
+        key = nums[i]
+        indx = i
+
+        for j in range(i+1,n):
+            if nums[j] > key:
+                key = nums[j]
+                indx = j
+        
+        nums[i],nums[indx] = nums[indx], nums[i]
+
+    return nums
+
+
+nums = [1, 2, 3, 5]
+
+print(f"Given array: {nums}")
+print(f"Sorted array: {selection_sort(nums)}")
