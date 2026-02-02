6bfbe2fe38dd243a446f7e3e004e358d0d91a264
aryanpatel10 <aaryanpatel013@gmail.com>
2026-02-03 01:06:01 +0530

    Return the index of first occurence of the Target element in the array

--- Arrays/linearSearch.py
+++ Arrays/linearSearch.py
@@ -1,3 +1,5 @@
+#Input: nums = [2,3,4,5,3,2], taget = 3
+#Output: 1 ; Return the smallest index where the target appears in the array.
 def linearSearch(nums,target):
     n = len(nums)
     for i in range(n):
