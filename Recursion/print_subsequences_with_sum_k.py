00db0a241d8337579991a5c77059eabeea0d4873
aryanpatel10 <aaryanpatel013@gmail.com>
2026-02-12 23:57:47 +0530

    Different pattern of subsequence problem

--- Recursion/print_subsequences_with_sum_k.py
+++ Recursion/print_subsequences_with_sum_k.py
@@ -23,3 +23,36 @@ def subsequence(indx,nums,subs,curr_sum,k):
 nums = [1,2,1]
 k = 2
 subsequence(0,nums,[],0,k)
+
+
+
+#                           TYPE 2: PRINT ANY ONE SUBSEQUENCE WITH SUM = K
+
+def subsequence(indx,nums,subs,curr_sm,k):
+    n = len(nums)
+    # BASE CASE
+    if indx == n:
+        if curr_sm == k:
+            print(subs)
+            return True
+        return False
+    
+    # PICK ELEMENT
+    subs.append(nums[indx])
+    curr_sm += nums[indx]
+    if subsequence(indx+1,nums,subs,curr_sm,k):
+        return True
+
+    # BACK TRACK
+    subs.pop()
+    curr_sm -= nums[indx]
+
+    # NOT PICK ELEMENT
+    if subsequence(indx+1,nums,subs,curr_sm,k):
+        return True
+    return False
+
+nums = [1,2,1]
+k = 2
+subsequence(0,nums,[],0,k)
+
