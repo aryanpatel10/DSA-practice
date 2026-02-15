9d7c34705b2c6350c60a721bd785bce305008e41
aryanpatel10 <aaryanpatel013@gmail.com>
2026-02-15 12:51:48 +0530

    creating valid combination from 1 to 9 to get the sum

---
+++ Recursion/combinationSum3.py
@@ -0,0 +1,39 @@
+#                                   COMBINATION SUM 3
+
+"""
+Find the combination of 'k' numbers whose sum up to give 'n'. Return the list of all possible combinations.
+Combinaiton should satisfy these condition:
+1. Choose numbers from 1 to 9 only
+2. Numbers should be used only for once
+3. No duplicates are alloud as the output
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Explanation:
+1 + 2 + 4 = 7
+There are no other valid combinations.
+"""
+
+def combinationSum(k,n):
+    res = []
+    
+    def combination(indx,target,combi):
+        # BASE CASE
+        if len(combi) == k:
+            if target == 0:
+                res.append(combi[:])
+            return
+        
+        for num in range(indx,10):
+            if num > target:
+                break
+
+            combi.append(num)
+            combination(num+1,target-num,combi)
+            combi.pop()
+    combination(1,n,[])
+    return res
+
+k = 3
+n = 7
+print(combinationSum(k,n))
